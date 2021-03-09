30e6eb9264233382c4730e9f599500997ea4f862
Unknown <unknown@example.com>
2021-03-09 15:39:14 +0200

    Complete comit la primul check-point

--- App.js
+++ App.js
@@ -1,134 +1,27 @@
 
 import React from 'react';
 import {
-   StyleSheet,
-   SafeAreaView,
-   Platform,
-   StatusBar,
-   View,
-   Image,
-   Text,
-   ImageBackground,
-   Dimensions,
+
+  View,
   } from 'react-native';
 
+import Card from './apps/components/Card';
+
 export default function App() {
-
-    // return(
+  return(
+    <View style={{
+      backgroundColor:'gray',
+      padding:20,
+      paddingTop:100,
       
-    //     <View style={{
-    //       backgroundColor: 'black',
-    //       flex:1,
-          
-    //     }}>
-    //       <View style={{
-    //         backgroundColor: 'cyan',
-    //         width:50,
-    //         height:50,
-    //         top:10,
-    //         right: 10,
-    //         paddingTop : Platform.OS ? StatusBar:0,
-    //         position: "absolute",
-    //       }}
-    //       />
-    //       <View style={{
-    //         backgroundColor: 'tomato',
-    //         width:50,
-    //         height:50,
-    //         top:10,
-    //         left: 10,
-    //         paddingTop : Platform.OS ? StatusBar:0,
-    //         position: "absolute",
-    //       }}/>
-        
-    //       <View style={{
-    //         justifyContent: 'center',
-    //         alignItems: 'center',
-    //         height: '100%',
-    //       }}>
-    //         <Image
-    //         style={{
-    //           width:400,
-    //           height:400,
-              
-    //            flex:-1
-              
-    //         }}
-    //           source={require('./assets/logo.png')}
-    //         />
-    //       </View>
-    //     </View> 
+    }}>
+      <Card
       
-    // )
+      image={require('./apps/assets/logo.png')}
+      title={"The jacket is real"}
+      subtitle={"100%"}
+      />
 
-    return(
-      <View style={{
-        backgroundColor : 'yellow',
-        flex: 1,
-        top : Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-      }}>
-        
-        <ImageBackground style={{
-        
-        flex:1,
-        flexDirection : 'column',
-        alignItems : 'center',
-        justifyContent:'flex-end',
-        
-          height :400,
-          //width : "100%",
-      
-
-        }}
-        source={require('./assets/logo.png')}
-        >
-        
-          <View style={{
-            flexDirection: 'column',
-            alignSelf: 'center',
-            bottom: "50%",
-          }}>
-            <Image style={{
-                
-                  height : 100,
-                  width :100,
-                  alignSelf : 'center',
-                }}
-                source={require('./assets/logo.png')}
-                />
-            <Text style={{
-                  fontSize :20,
-                fontWeight :'bold',
-                alignSelf : 'center',
-                  
-                }}>Pagine de pornire foarte jmechera</Text>   
-
-
-          </View>
-          <View style={{
-            justifyContent:'flex-end',
-            alignItems: 'stretch',
-            
-         
-          }}>
-            <View style={{
-              backgroundColor : 'orange',          
-              height : 50,
-              width : 1000,
-            }}/>
-            <View style={{
-              backgroundColor : 'green',          
-              height : 75,
-              //width :50,
-              
-            }}/>
-          
-          
-          </View>
-      
-        </ImageBackground>
-
-      </View>
-  
-    )
+    </View>    
+  )
 }
